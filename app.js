c60e548bd473b8d15c4725cf7c8ec5a9ecce5001
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-08-22 17:31:25 +0100

    added test with jest

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 require("dotenv").config();
 const config = require("config");
+require("express-async-errors");
+const winston = require("winston");
+
+
+
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: JWT not defined");
@@ -32,6 +37,8 @@ app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/movies", movies);
 app.use("/api/auth", auth);
+
+ 
 
 app.listen(7000, () => {
   console.log("server is running on port 7000");
